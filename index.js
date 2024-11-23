01f46eab47af290a5b8de29cb5794da83b7e3392
manuel <40071285+manuelperez0000@users.noreply.github.com>
2024-11-23 07:47:21 -0600

    Actualizar index.js

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ app.use(express.json())
 
 app.get('/', (req, res) => res.send("Welcome 34"))
 
-app.listen(5000, () => console.log(`Servidor escuchando en el puerto 5000`))
-  
+app.listen(5000, () => { 
+console.log(`Servidor escuchando en el puerto 5000`))
+  }
