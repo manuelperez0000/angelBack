52e29540beb83a602771bf5cb23cc903405f1c3b
manuel <40071285+manuelperez0000@users.noreply.github.com>
2024-11-23 08:35:03 -0600

    Actualizar index.js

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-require('dotenv')
+require('dotenv').config({ path: '.env' })
+const express = require('express');
+const cors = require('cors')
+const app = express();
 const PORT = process.env.PORT || 5000;
-const express = require('express')
-const app = express()
+const morgan = require('morgan');
 
+app.use(morgan('dev'))
+app.use(cors())
 app.use(express.json())
 
-app.get('/', (req, res) => res.send("Welcome 34"))
+app.get('/', (req, res) => res.send("Welcome 2"))
 
-app.listen(PORT, () => console.log(`Servidor escuchando en el puerto 5000`))
+
+app.listen(PORT, () => console.log(`Servidor escuchando en el puerto ${PORT}`))
