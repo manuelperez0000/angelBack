c140dbe4d947902fde9602d6b6d0fd919c5b2419
manuel <40071285+manuelperez0000@users.noreply.github.com>
2024-11-23 07:55:00 -0600

    Actualizar index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require('dotenv').config({ path: '.env' })
-
+const PORT = process.env.PORT || 5000;
 const express = require('express')
 const app = express()
 
@@ -7,6 +7,6 @@ app.use(express.json())
 
 app.get('/', (req, res) => res.send("Welcome 34"))
 
-app.listen(5000, () => { 
+app.listen(PORT, () => { 
 console.log(`Servidor escuchando en el puerto 5000`))
   }
